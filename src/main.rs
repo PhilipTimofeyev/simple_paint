5d0f4f667061f0afbd92c82c35a8a0bae0a23843
Philip Timofeyev <PhilipTimofeyev@gmail.com>
2026-02-03 11:01:20 -0800

    fix: update name of app

--- src/main.rs
+++ src/main.rs
@@ -18,7 +18,7 @@ fn main() -> eframe::Result {
         ..Default::default()
     };
     eframe::run_native(
-        "eframe template",
+        "Simple Paint",
         native_options,
         Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
     )
